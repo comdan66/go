88500c706a2281c39ae086d4cb0b9d95aa933b23
OA Wu <comdan66@gmail.com>
2015-07-20 18:13:13 +0800

    Finish some site layout.

--- application/cell/controllers/main_cell.php
+++ application/cell/controllers/main_cell.php
@@ -86,7 +86,7 @@ class Main_cell extends Cell_Controller {
   // public function _cache_search () {
   //   return array ('time' => 60 * 60, 'key' => null);
   // }
-  public function search ($is_search = false) {
+  public function search ($is_search = true) {
     $limit = 15;
     $d4 = array (array (231, 109, 102), array (244, 177, 60), array (242, 224, 68), array (183, 232, 94), array (110, 192, 166), array (122, 174, 218), array (56, 121, 217), array (103, 58, 183), array (156, 39, 176), array (163, 145, 199), array (255, 192, 203), array (210, 129, 167), array (0, 0, 0), array (128, 128, 128), array (255, 255, 255), array (255, 255, 255));
 
